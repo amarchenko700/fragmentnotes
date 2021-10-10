3d8e7dde681b352f68bdea2337fcfb45bdd9d6ff
amarchenko700 <lexx700@yandex.ru>
2021-10-10 15:03:44 +0300

    lesson6: доделал фрамент, работающий со списком заметок

--- app/src/main/java/com/example/fragmentnotes/ui/NoteListFragment.java
+++ app/src/main/java/com/example/fragmentnotes/ui/NoteListFragment.java
@@ -1,7 +1,6 @@
 package com.example.fragmentnotes.ui;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -20,30 +19,24 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fragmentnotes.R;
 import com.example.fragmentnotes.domain.NoteEntity;
-import com.example.fragmentnotes.domain.NotesRepo;
 import com.example.fragmentnotes.impl.NotesRepoImpl;
 
 public class NoteListFragment extends Fragment {
 
     private RecyclerView recyclerView;
-    private Toolbar toolbar;
-    private NotesRepo notesRepo = new NotesRepoImpl();
     private NotesAdapter adapter = new NotesAdapter();
     private NoteEntity noteEntity;
+    private NotesRepoImpl notesRepo;
     private ControllerNoteList controllerNoteList;
 
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
-        if(context instanceof ControllerNoteList){
+        if (context instanceof ControllerNoteList) {
             controllerNoteList = (ControllerNoteList) context;
-        }else {
+        } else {
             throw new IllegalStateException("Activity must implement NoteListFragment.ControllerNoteList");
         }
-    }
-
-    public NoteListFragment() {
-        fillRepoByTestValuesRepo();
     }
 
     @Nullable
@@ -54,15 +47,18 @@ public class NoteListFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        initToolbar(view);
-        initRecyclerView(view);
         super.onViewCreated(view, savedInstanceState);
+        initRecyclerView();
     }
 
     @Override
-    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-        inflater.inflate(R.menu.note_list_menu, menu);
-        super.onCreateOptionsMenu(menu,inflater);
+    public void onDestroy() {
+        controllerNoteList = null;
+        super.onDestroy();
+    }
+
+    public void setNotesRepo(NotesRepoImpl notesRepo) {
+        this.notesRepo = notesRepo;
     }
 
     @Override
@@ -75,55 +71,30 @@ public class NoteListFragment extends Fragment {
         return super.onOptionsItemSelected(item);
     }
 
-    private void initToolbar(View view) {
-        toolbar = view.findViewById(R.id.toolbar);
-        /*
-        Правильно ли я здесь работаю с установкой меню?
-         */
-        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
-        setHasOptionsMenu(true);
-    }
-
-    private void initRecyclerView(View view) {
-        recyclerView = view.findViewById(R.id.recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setAdapter(adapter);
-        adapter.setOnItemClickListener(this::onItemClick);
-        adapter.setData(notesRepo.getNotes());
-    }
-
     private void onItemClick(NoteEntity item) {
         controllerNoteList.openNoteItem(item);
     }
 
-    public NotesRepo getNotesRepo() {
-        return notesRepo;
+    private void initRecyclerView() {
+        recyclerView = getView().findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
+        adapter.setOnItemClickListener(this::onItemClick);
+        setRecyclerViewAdapterData();
     }
 
-    private void fillRepoByTestValuesRepo() {
-        notesRepo.createNote(new NoteEntity("День 1", "Решил заниматься андроидом"));
-        notesRepo.createNote(new NoteEntity("День 2", "Записался на GeekBrains"));
-        notesRepo.createNote(new NoteEntity("День 3", "И пошла жара"));
-        notesRepo.createNote(new NoteEntity("День 4", "Теперь даже некогда отдыхать"));
-        notesRepo.createNote(new NoteEntity("День 5", "Только то и делаю, что что-то клипаю, клипаю и клипаю"));
-        notesRepo.createNote(new NoteEntity("День 6", "Иногда некогда покушать"));
-        notesRepo.createNote(new NoteEntity("День 7", "Но в целом учиться - очень круто"));
-        notesRepo.createNote(new NoteEntity("День 8", "Пишем на Java, скоро Kotlin - в общем мы крутые перцы "));
-        notesRepo.createNote(new NoteEntity("День 9", "Все отлично"));
-        notesRepo.createNote(new NoteEntity("День 10", "Все замечательно"));
-        notesRepo.createNote(new NoteEntity("День 11", "Это такой типа дневник"));
-        notesRepo.createNote(new NoteEntity("День 12", "Почти все"));
-        notesRepo.createNote(new NoteEntity("День 13", "Еще не все"));
-        notesRepo.createNote(new NoteEntity("День 14", "Теперь все"));
+    public void setRecyclerViewAdapterData() {
+        adapter.setData(notesRepo.getNotes());
     }
 
-    @Override
-    public void onDestroy() {
-        controllerNoteList = null;
-        super.onDestroy();
-    }
-
-    public interface ControllerNoteList{
+    public interface ControllerNoteList {
         void openNoteItem(NoteEntity item);
     }
+
+    public static NoteListFragment newInstance(NotesRepoImpl notesRepository) {
+        NoteListFragment fragment = new NoteListFragment();
+        fragment.setNotesRepo(notesRepository);
+        return fragment;
+    }
+
 }
