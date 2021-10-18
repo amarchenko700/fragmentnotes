314c0ec8a7236ea765fc7bcae79f5f68aebfba32
amarchenko700 <lexx700@yandex.ru>
2021-10-18 20:42:03 +0300

    lesson9: Added context menu handlers

--- app/src/main/java/com/example/fragmentnotes/ui/NoteListFragment.java
+++ app/src/main/java/com/example/fragmentnotes/ui/NoteListFragment.java
@@ -4,16 +4,12 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -22,15 +18,16 @@ import com.example.fragmentnotes.R;
 import com.example.fragmentnotes.domain.NoteEntity;
 import com.example.fragmentnotes.impl.NotesRepoImpl;
 
-import java.util.List;
-
 public class NoteListFragment extends Fragment {
 
+    private static final String KEY_REPO = "KEY_REPO";
     private RecyclerView recyclerView;
-    private NotesAdapter adapter = new NotesAdapter();
+    private final NotesAdapter adapter = new NotesAdapter();
     private NoteEntity createdNoteEntity;
-    private final NotesRepoImpl notesRepo;
+    private NotesRepoImpl notesRepo;
     private ControllerNoteList controllerNoteList;
+    private NoteEntity clickedNote;
+    private int indexClicked;
 
     @Override
     public void onAttach(@NonNull Context context) {
@@ -51,7 +48,13 @@ public class NoteListFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        registerForContextMenu(view);
+
+        Bundle args = getArguments();
+        assert args != null;
+        notesRepo = args.getParcelable(KEY_REPO);
         initRecyclerView();
+        setAdapterData();
         if(createdNoteEntity != null){
             scrollToLast();
         }
@@ -63,16 +66,37 @@ public class NoteListFragment extends Fragment {
         super.onDestroy();
     }
 
-    public NoteListFragment(NotesRepoImpl notesRepo) {
-        this.notesRepo = notesRepo;
+    @Override
+    public void onCreateContextMenu(@NonNull ContextMenu menu, @NonNull View v, @Nullable ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        requireActivity().getMenuInflater().inflate(R.menu.note_list_context_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.delete_note && clickedNote != null){
+            notesRepo.removeNote(clickedNote);
+            clickedNote = null;
+            //setAdapterData();
+            adapter.notifyItemRemoved(indexClicked);
+            return true;
+        }
+        return super.onContextItemSelected(item);
     }
 
     public void setCreatedNoteEntity(NoteEntity createdNoteEntity) {
         this.createdNoteEntity = createdNoteEntity;
     }
 
-    private void onItemClick(NoteEntity item) {
-        controllerNoteList.openNoteItem(item);
+    private void onItemClick(NoteEntity item, int position) {
+        controllerNoteList.openNoteItem(item, position, false);
+    }
+
+    private boolean onItemContextClick(View v, NoteEntity item, int position) {
+        clickedNote = item;
+        indexClicked = position;
+        v.showContextMenu();
+        return true;
     }
 
     private void initRecyclerView() {
@@ -80,11 +104,20 @@ public class NoteListFragment extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(adapter);
         adapter.setOnItemClickListener(this::onItemClick);
-        setRecyclerViewAdapterData();
+        adapter.setOnItemContextClickListener(this::onItemContextClick);
     }
 
-    public void setRecyclerViewAdapterData() {
+    public void setAdapterData() {
         adapter.setData(notesRepo.getNotes());
+    }
+
+    public void notifyItemChanged(int position){
+        adapter.notifyItemChanged(position);
+    }
+
+    public void notifyItemInserted(int position){
+        adapter.notifyItemInserted(position);
+        setAdapterData();
     }
 
     public void scrollToLast(){
@@ -95,11 +128,14 @@ public class NoteListFragment extends Fragment {
     }
 
     public interface ControllerNoteList {
-        void openNoteItem(NoteEntity item);
+        void openNoteItem(NoteEntity item, int position, boolean isNew);
     }
 
     public static NoteListFragment newInstance(NotesRepoImpl notesRepository) {
-        NoteListFragment fragment = new NoteListFragment(notesRepository);
+        NoteListFragment fragment = new NoteListFragment();
+        Bundle args = new Bundle();
+        args.putParcelable(KEY_REPO, notesRepository);
+        fragment.setArguments(args);
         return fragment;
     }
 
